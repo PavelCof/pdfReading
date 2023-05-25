6e661a221096f9e8fe7461567b74e1cf85bc41ef
Pavel Cof <cubalab@gmail.com>
2023-05-25 15:15:36 +0300

    edit readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PDFViewer from './PdfReader/PDFViewer';
+ import { pdfjs } from 'react-pdf';
 
 function App() {
+ 
+// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+
+
+  const pdfFile = '/pdfs/123.pdf';
+
+
   return (
+    <>
+ 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <PDFViewer file={pdfFile}    />
     </div>
+    </>
+
   );
 }
 
---
+++ src/PdfReader/PDFViewer.tsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from 'react';
+import { Document, Page ,pdfjs} from 'react-pdf';
+
+
+interface PDFViewerProps {
+  file: string; // Путь к PDF-файлу
+  page?: number; // Путь к PDF-файлу
+ 
+}
+
+
+function PDFViewer({ file,page }:PDFViewerProps){
+  pdfjs.GlobalWorkerOptions.workerSrc ="./pdfworker.js" // этот файл нужно разместить в папке public
+
+
+  const [pagesCount,setPagesCount] = useState<number>(0);
+  const [pageNow,setPageNow] = useState<number>(1);
+  const pdfPath = process.env.PUBLIC_URL + file;
+
+  function pageBack() {
+    if(pageNow-1 > 0){
+      setPageNow(pageNow-1)
+
+      
+    }
+  }
+  function pageNext() {
+    if(pageNow+1 <= pagesCount){
+      setPageNow(pageNow+1)
+ 
+      
+    }
+  }
+
+
+  function getNumPages(pdfPath:string) {
+    return new Promise((resolve, reject) => {
+      const loadingTask = pdfjs.getDocument(pdfPath);
+      loadingTask.promise
+        .then((pdf) => {
+          resolve(pdf.numPages);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  }
+  
+
+  useEffect(() => {
+    getNumPages(pdfPath)
+      .then((pages) => {
+        console.log(pages,typeof pages );
+        typeof pages === 'number' && setPagesCount(pages);
+
+      })
+      .catch((error) => {
+        console.error('Error getting number of pages:', error);
+      });
+  }, [file,pdfPath]);
+  
+  return (
+    <div >
+        <div className='flex-row center'>       
+             <button onClick={pageBack}>Назад</button> <div className='m-3 p-3'>{pageNow}</div>  <button onClick={pageNext}>Вперёд</button>  <div className='m-3 p-3'> всего страниц : {pagesCount}</div> 
+        </div> 
+        <div className='flex-row center'>  
+          <Document file={pdfPath} className=' w858'>
+            <Page pageNumber={pageNow} />
+          </Document>
+        </div> 
+    </div>
+  );
+};
+
+export default PDFViewer;
+
+
